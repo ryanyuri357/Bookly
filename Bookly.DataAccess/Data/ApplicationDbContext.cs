db662fa995853b1a2df08f0160229d4140b38a0a
ryanyuri357 <ryan.yuri357@gmail.com>
2021-11-21 11:53:35 -0600

    Migrated Setup to N Tier Application

---
+++ Bookly.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Bookly.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookly.DataAccess
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
