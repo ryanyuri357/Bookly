1419b1231938f640e9bf0c534b5fc415bba88a52
ryanyuri357 <ryan.yuri357@gmail.com>
2021-11-22 08:55:28 -0600

    Create Product - Add Category Dropdown

--- Bookly.DataAccess/Data/ApplicationDbContext.cs
+++ Bookly.DataAccess/Data/ApplicationDbContext.cs
@@ -12,5 +12,6 @@ namespace Bookly.DataAccess
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<CoverType> CoverTypes { get; set; }
+        public DbSet<Product> Products { get; set; }
     }
 }
---
+++ BooklyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,106 @@
+﻿using Bookly.DataAccess;
+using Bookly.DataAccess.Repository.IRepository;
+using Bookly.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BooklyWeb.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<CoverType> objCoverTypeList = _unitOfWork.CoverType.GetAll(); 
+            return View(objCoverTypeList);
+        }
+
+        //GET
+        public IActionResult Upsert(int? id)
+        {
+            Product product = new();
+            IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category.GetAll().Select(
+                u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString(),
+                });
+            IEnumerable<SelectListItem> CoverTypeList = _unitOfWork.CoverType.GetAll().Select(
+                u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString(),
+                });
+
+            if (id == null || id == 0)
+            {
+                //create product
+                ViewBag.CategoryList = CategoryList;
+                return View(product);
+            }
+            else
+            {
+                //update product
+            }
+
+            return View(product);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(CoverType obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.CoverType.Update(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "CoverType updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var CoverTypeFromDbFirst = _unitOfWork.CoverType.GetFirstOrDefault(u => u.Id == id);
+
+            if (CoverTypeFromDbFirst == null)
+            {
+                return NotFound();
+            }
+
+            return View(CoverTypeFromDbFirst);
+        }
+
+        //POST
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _unitOfWork.CoverType.GetFirstOrDefault(u => u.Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _unitOfWork.CoverType.Remove(obj);
+            _unitOfWork.Save();
+            TempData["success"] = "CoverType deleted successfully";
+            return RedirectToAction("Index");
+            
+        }
+    }
+}
