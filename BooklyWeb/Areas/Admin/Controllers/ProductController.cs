56adde68c6a0a587153817fc493485b1fabea464
ryanyuri357 <ryan.yuri357@gmail.com>
2021-11-22 15:13:47 -0600

    Product List Edit Delete Buttons

--- BooklyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BooklyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -128,7 +128,7 @@ namespace BooklyWeb.Controllers
         [HttpGet]
         public IActionResult GetAll()
         {
-            var productList = _unitOfWork.Product.GetAll();
+            var productList = _unitOfWork.Product.GetAll(includeProperties:"Category,CoverType");
 
             return Json(new { data = productList });
         }
