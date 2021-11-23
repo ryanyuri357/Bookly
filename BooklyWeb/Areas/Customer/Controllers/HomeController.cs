e26615c828319798ce9110e9f14fcba84a0a701a
ryanyuri357 <ryan.yuri357@gmail.com>
2021-11-23 16:33:55 -0600

    Home Page

--- BooklyWeb/Areas/Customer/Controllers/HomeController.cs
+++ BooklyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
-﻿using Bookly.Models;
+﻿using Bookly.DataAccess.Repository.IRepository;
+using Bookly.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Collections.Generic;
 
 namespace BooklyWeb.Controllers
 {
@@ -8,15 +10,18 @@ namespace BooklyWeb.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category,CoverType");
+            return View(productList);
         }
 
         public IActionResult Privacy()
